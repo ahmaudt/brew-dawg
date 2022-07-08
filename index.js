a1a8ccceaf80cde4d71067e5d1bfd0e025f2308d
theNicomachean <ahmaud@timaeus.pro>
2022-07-07 22:57:23 -0400

    created 2nd eventlistener for results

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content Now Loaded');
     const form = document.querySelector('form');
+
+
+    let selection = document.querySelector('select');
+    selection.onchange = searchByInput;
+
+    function setAttrs(element, attrs) {
+        Object.keys(attrs).forEach(attr => {
+            element.setAttribute(attr, attrs[attr]);
+        })
+    }
+    
+
+    function searchByInput(e) {
+        let searchBy = document.getElementById('searchBy');
+        let searchID = searchBy.selectedOptions[0].value;
+        let searchInput = document.createElement('input');
+        let searchBtn = document.createElement('button');
+        searchBtn.setAttribute('type', 'submit');
+        searchBtn.classList.add('btn', 'btn-primary', 'rounded-1', 'mt-3');
+        searchBtn.innerText = 'Search';
+        searchInput.setAttribute('type', 'text');
+        searchInput.id = searchID.toString()
+        searchInput.classList.add('form-control-sm', searchID.toString(), 'rounded-0', 'mt-2');
+        console.log(searchInput.classList[1]);
+        
+        // searchInput.id = `${searchID}`;
+        // searchInput.class = 'form-control';
+        if (searchBy.nextElementSibling.tagName === 'INPUT') {
+          let currentInput = document.querySelector('input');
+          let currentBtn = document.querySelector('button');
+            currentInput.replaceWith(searchInput);
+            // currentBtn.replaceWith(searchBtn);
+
+        } else {
+            searchBy.after(searchInput);
+            // searchInput.after(searchBtn);
+        }
+      }
     
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -12,14 +50,10 @@ document.addEventListener('DOMContentLoaded', () => {
         function getParams(input) {
             let url = '';
             let userInput = encodeURIComponent(input.value);
-            if (input.id === "searchState") {
+            if (input.classList[1] === "State") {
                 url = `by_state=${userInput}`
-            } else if (input.id === "searchCity") {
+            } else if (input.classList[1] === "City") {
                 url = `by_city=${userInput}`
-            } else if (input.id = "searchName") {
-                url = `by_name=${userInput}`
-            } else {
-                url = `by_type=${userInput}`
             }
             return url;
         }
@@ -37,7 +71,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     breweries.forEach(brewery => {
                         brewery.forEach(brew => {
                             const brewAnchor = document.createElement('a');
-                            brewAnchor.href = `${brew.website_url}`;
+                            const anchorAttributes = {
+                                "data-bs-toggle": 'modal',
+                                "data-bs-target": `#${brew.name[0]+brew.name[1]+brew.name[2]+brew.name[3]+brew.name[4]+brew.name[5]+brew.name[6]}`
+                            }
+                            setAttrs(brewAnchor, anchorAttributes);
+                            brewAnchor.addEventListener('shown.bs.modal', (e) => {
+                                 e.target.focus();
+                            })
+                            let modal = document.createElement('div');
+                            modal.innerHTML = `<div class='modal' id=${brew.name[0]+brew.name[1]+brew.name[2]+brew.name[3]+brew.name[4]+brew.name[5]+brew.name[6]} tabindex='-1'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>${brew.name}</h5><button type='button' class='close' data-bs-dismiss='modal'>&times;</button></div><div class='modal-body'><p>View their homepage<a href=${brew.website_url}>here</a></p></div><div class='modal-footer'><button type='button' class='btn btn-primary' data-bs-dismiss='modal'>Close</button></div></div></div></div>`
+
+                            // brewAnchor.href = `${brew.website_url}`;
                             const flexDiv = document.createElement('div');
                             const innerDiv = document.createElement('div');
                             brewAnchor.classList.add("list-group-item", "list-group-item-action", "d-flex", 'gap-3', 'py-3');
@@ -54,7 +99,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             console.log(addr)
 
                             innerFlexContent.innerHTML = `<a href='https://www.google.com/maps?saddr=My+Location&daddr=${addr}'>${brew.city}, ${brew.state} <i class="fa-solid fa-map-location-dot"></i></a>`;
-                            const brewType = () => {e
+                            const brewType = () => {
                                 if (brew.brewery_type === 'micro') {
                                     return `<h5 class='mb-0'><i class="fa-solid fa-beer-mug pe-1"></i>${brew.name}</h5><p class='mb-0 opacity-75'>${brew.brewery_type}</p>`
                                 } else if (brew.brewery_type === 'brewpub') {
@@ -77,6 +122,9 @@ document.addEventListener('DOMContentLoaded', () => {
                             innerDiv.innerHTML = brewType();
                             flexDiv.append(innerDiv)
                             innerDiv.after(innerFlexContent);
+                            resultsDiv.before(modal);
+                            // const popover = document.querySelectorAll('[data-bs-toggle="popover"]');
+                            // const popoverIntialize = new bootstrap.Popover(popover);
                         })
                     })
                 }}
